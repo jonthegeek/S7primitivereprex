7d77ab48d7f728f8bbea515af8db788a0447ab15
Jon Harmon <jonthegeek@gmail.com>
2023-09-11 13:22:21 -0500

    S7 methods vs S3 methods.
    I don't know if this matters, but it's interesting. I'm leaning toward it mattering since I'm getting different results from testing vs running locally.

--- R/my_class.R
+++ R/my_class.R
@@ -1,9 +1,23 @@
+#' @export
 my_class <- S7::new_class(
   "my_class",
   package = "S7primitivereprex",
   parent = S7::class_character
 )
 
+#' @export
+my_other_class <- S7::new_class(
+  "my_other_class",
+  package = "S7primitivereprex",
+  parent = S7::class_character
+)
+
+#' @export
 S7::method(length, my_class) <- function(x) {
   1
+}
+
+#' @export
+`length.S7primitivereprex::my_other_class` <- function(x, ...) {
+  2
 }
